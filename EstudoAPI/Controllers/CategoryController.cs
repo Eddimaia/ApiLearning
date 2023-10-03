e6bf43a2bfe9f2a5b4017f6c39a69a00bc94a3b6
Edson Maia da Costa Moura <edson.maia@fadami.com.br>
2023-10-03 00:10:10 -0300

    feat: crud

--- EstudoAPI/Controllers/CategoryController.cs
+++ EstudoAPI/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using EstudoAPI.Data;
+using EstudoAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,13 +15,103 @@ namespace EstudoAPI.Controllers
         }
 
         [HttpGet("v1/categories")]
-        public IActionResult Get()
+        public async Task<IActionResult> GetAsync()
         {
-            var cartegories = _context.Categories
+            var cartegories = await _context.Categories
                 .AsNoTracking()
-                .ToList();
+                .ToListAsync();
 
             return Ok(cartegories);
+        }
+
+        [HttpGet("v1/categories/{id:int}")]
+        public async Task<IActionResult> GetByIdAsync([FromRoute] int id)
+        {
+            var category = await _context.Categories
+                .AsNoTracking()
+                .FirstOrDefaultAsync(cat => cat.Id.Equals(id));
+
+            return Ok(category);
+        }
+
+        [HttpPost("v1/categories")]
+        public async Task<IActionResult> PostAsync([FromBody] Category model)
+        {
+            try
+            {
+                await _context.Categories.AddAsync(model);
+                await _context.SaveChangesAsync();
+
+                return Created($"v1/categories/{model.Id}", model);
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, "MSG-E01.2 - Não foi possível incluir a categoria");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "MSG-E02 - Falha interna no servidor");
+            }
+
+        }
+
+        [HttpPut("v1/categories/{id:int}")]
+        public async Task<IActionResult> PutAsync(
+            [FromRoute] int id,
+            [FromBody] Category model)
+        {
+            try
+            {
+                var category = await _context.Categories
+                .AsNoTracking()
+                .FirstOrDefaultAsync(cat => cat.Id.Equals(id));
+
+                if (category is null)
+                    return NotFound();
+
+                category.Name = model.Name;
+                category.Slug = model.Slug;
+
+                _context.Categories.Update(category);
+                await _context.SaveChangesAsync();
+
+                return Ok(new { Result = "Atualizado com sucesso." });
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, "MSG-E01.3 - Não foi possível atualizar a categoria");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "MSG-E02 - Falha interna no servidor");
+            }
+        }
+
+        [HttpDelete("v1/categories/{id:int}")]
+        public async Task<IActionResult> DeleteAsync([FromRoute] int id)
+        {
+            try
+            {
+                var category = await _context.Categories
+                .AsNoTracking()
+                .FirstOrDefaultAsync(cat => cat.Id.Equals(id));
+
+                if (category is null)
+                    return NotFound();
+
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+
+                return Ok(new { Result = "Deletado com sucesso." });
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, "MSG-E01.4 - Não foi possível deletar a categoria");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "MSG-E02 - Falha interna no servidor");
+            }
         }
     }
 }
