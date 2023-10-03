55e7414dbe3da0d3c95446bdb8335f42cd441e3d
Edson Maia da Costa Moura <edson.maia@fadami.com.br>
2023-10-03 01:04:47 -0300

    refactor: validation

--- EstudoAPI/DTO/EditorCategoryDTO.cs
+++ EstudoAPI/DTO/EditorCategoryDTO.cs
@@ -1,8 +1,14 @@
-﻿namespace EstudoAPI.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EstudoAPI.DTO
 {
     public class EditorCategoryDTO
     {
+        [Required(ErrorMessage = "Name é obrigatório")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "Este campo deve conter entre 3 e 40 caracteres")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Slug é obrigatório")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "Este campo deve conter entre 3 e 40 caracteres")]
         public string Slug { get; set; }
     }
 }
